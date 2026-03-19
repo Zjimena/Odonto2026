c157ed62246fbadd105ec528cfc5ec9e1f3bb2f7
Jimena Escudero <78107591+Zjimena@users.noreply.github.com>
2026-03-19 10:16:52 -0600

    Update imports and model configuration in app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ if not firebase_admin._apps:
 db = firestore.client()
 
 genai.configure(api_key=GOOGLE_API_KEY)
-modelo = genai.GenerativeModel('models/gemini-1.5-flash')
+modelo = genai.GenerativeModel('models/gemini-pro')
 
 
 
